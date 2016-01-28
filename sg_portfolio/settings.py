4a3dde83fd392c1da3ef8254b3c95bc1229dd47b
Sylvestre67 <sylvestre.gug@gmail.com>
2016-01-28 00:53:12 -0500

    Media

--- sg_portfolio/settings.py
+++ sg_portfolio/settings.py
@@ -129,7 +129,7 @@ if os.getenv('DATABASE_URL'):
     #ADMIN_MEDIA_PREFIX = STATIC_URL + 'grappelli/'
 
     # Media files on S3 configuration
-    MEDIA_ROOT = ''
+    MEDIA_ROOT = 'media'
     MEDIAFILES_LOCATION = 'media'
     MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
     DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
