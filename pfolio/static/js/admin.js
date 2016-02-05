bee441e69837d93113910493bce85c7de5fb55ed
Sylvestre Gug <sylvestre@outriggermedia.com>
2016-02-05 16:45:53 -0500

    Reinitialize grappelli

--- pfolio/static/js/admin.js
+++ pfolio/static/js/admin.js
@@ -1,6 +1,28 @@
+function CFileBrowser(field_name, url, type, win) {
+
+    var cmsURL = '/admin/filebrowser/browse/?pop=2';
+    cmsURL = cmsURL + '&type=' + type;
+
+    tinyMCE.activeEditor.windowManager.open({
+        file: cmsURL,
+        width: 980,  // Your dimensions may differ - toy around with them!
+        height: 500,
+        resizable: 'yes',
+        scrollbars: 'yes',
+        inline: 'no',  // This parameter only has an effect if you use the inlinepopups plugin!
+        close_previous: 'no'
+    }, {
+        window: win,
+        input: field_name
+    });
+    return false;
+}
+
 tinymce.init({
   selector: 'textarea',
   height: 500,
+  // Callbackss
+  file_browser_callback: CFileBrowser,
   plugins: [
         "advlist autolink lists link image charmap print preview anchor",
         "searchreplace visualblocks code fullscreen",
